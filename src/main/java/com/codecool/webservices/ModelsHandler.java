53c753f33820430426c154fceb99cae18c3da76f
saturator22 <f.szczeski@gmail.com>
2018-07-31 18:30:33 +0200

    Bugfixes in handlers

--- src/main/java/com/codecool/webservices/ModelsHandler.java
+++ src/main/java/com/codecool/webservices/ModelsHandler.java
@@ -1,34 +1,48 @@
-//package com.codecool.webservices;
-//
-//import com.codecool.model.Model;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.Persistence;
-//import javax.persistence.Query;
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
-//import java.util.Collection;
-//
-//@Path("/producers/{producerid}/brands/{brandid}/models")
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON)
-//public class ModelsHandler {
-//    EntityManagerFactory emf = Persistence.createEntityManagerFactory("carchek");
-//    EntityManager em = emf.createEntityManager();
-//
-//    @GET
-//    public Collection<Model> listModels(@PathParam("brandid") Integer brandId) {
+package com.codecool.webservices;
+
+import com.codecool.model.Model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Path("producers/1/brands/2/models")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ModelsHandler {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("carcheck");
+    EntityManager em = emf.createEntityManager();
+
+    @GET
+    public List<Model> listModels() {
 //        Query query = em.createQuery("SELECT m FROM model m WHERE brand_id=:id");
 //        query.setParameter("id", brandId);
 //
 //        Collection<Model> modelsCollection = query.getResultList();
-//        return modelsCollection;
-//    }
-//
+
+        Model model1 = new Model("Poldon", "MOCNY W UJ", 1000);
+        Model model2 = new Model("Malczan", "SLABY W UJ", 24);
+
+        em.getTransaction().begin();
+        em.persist(model1);
+        em.persist(model2);
+        em.getTransaction().commit();
+
+        List<Model> modelsCollection = new ArrayList<>();
+        modelsCollection.add(model1);
+        modelsCollection.add(model2);
+        return modelsCollection;
+    }
+
 //    @GET
 //    @Path("{id}")
 //    public Model getModelById(@PathParam("id") Integer modelId) {
 //        return em.find(Model.class, modelId);
 //    }
-//}
+}
--- src/main/java/com/codecool/webservices/ProducersHandler.java
+++ src/main/java/com/codecool/webservices/ProducersHandler.java
@@ -1,6 +1,48 @@
-//package com.codecool.webservices;
-//
-//import javax.ws.rs.GET;
-//
-//public class ProducersHandler {
-//}
+package com.codecool.webservices;
+
+import com.codecool.model.Producer;
+import com.codecool.services.ProducerService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/producers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ProducersHandler {
+
+    private ProducerService producerService = new ProducerService();
+
+    @GET
+    public List<Producer> getProducers() {
+        return producerService.getAllProducers();
+    }
+
+    @POST
+    public Producer addProducer(Producer producer) {
+        return producerService.addProducer(producer);
+    }
+
+    @PUT
+    @Path("/{producerId}")
+    public Producer updateProducer(@PathParam("producerId") long id, Producer producer) {
+        producer.setId(id);
+        return producerService.updateProducer(producer);
+    }
+
+    @DELETE
+    @Path("/{producerId}")
+    public void deleteProducer(@PathParam("producerId") long id) {
+        producerService.removeProducer(id);
+    }
+
+
+    @GET
+    @Path("/{producerId}")
+    public Producer getProducer(@PathParam("producerId") long id) {
+        return producerService.getProducer(id);
+    }
+}
+
+
