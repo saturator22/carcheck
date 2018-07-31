bd7764b6e04aa0260e7cf6bbf270f22ed6f09afb
saturator22 <f.szczeski@gmail.com>
2018-07-31 11:52:27 +0200

    Annotate POJO Model with JPA annotations

--- src/main/java/com/codecool/model/Model.java
+++ src/main/java/com/codecool/model/Model.java
@@ -2,14 +2,14 @@ package com.codecool.model;
 
 import javax.persistence.*;
 
-@Entity
+@Entity(name = "model")
 public class Model{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "brandId")
-    private long brandId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Brand brand;
 
     private String name;
 
@@ -17,6 +17,59 @@ public class Model{
 
     private int horsePower;
 
-    
+    public Model() {
+
+    }
+
+    public Model(String name, String engine, int horsePower) {
+        this.name = name;
+        this.engine = engine;
+        this.horsePower = horsePower;
+    }
+
+    public Model(String name, String engine, int horsePower, Brand brand) {
+        this(name, engine, horsePower);
+        this.brand = brand;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
 }
 
