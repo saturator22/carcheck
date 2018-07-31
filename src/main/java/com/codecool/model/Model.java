e28f48adad075d5d7d8adc0499f7865df9dc29c2
saturator22 <f.szczeski@gmail.com>
2018-07-31 18:27:34 +0200

    Add Entities Model and Producer

--- src/main/java/com/codecool/model/Model.java
+++ src/main/java/com/codecool/model/Model.java
@@ -1,45 +1,47 @@
-//package com.codecool.model;
-//
-//import javax.persistence.*;
-//
-//@Entity(name = "model")
-//public class Model{
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long id;
-//
+package com.codecool.model;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity(name = "model")
+@XmlRootElement
+public class Model{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 //    @ManyToOne(cascade = CascadeType.ALL)
 //    private Brand brand;
-//
-//    private String name;
-//
-//    private String engine;
-//
-//    private int horsePower;
-//
-//    public Model() {
-//
-//    }
-//
-//    public Model(String name, String engine, int horsePower) {
-//        this.name = name;
-//        this.engine = engine;
-//        this.horsePower = horsePower;
-//    }
-//
+
+    private String name;
+
+    private String engine;
+
+    private int horsePower;
+
+    public Model() {
+
+    }
+
+    public Model(String name, String engine, int horsePower) {
+        this.name = name;
+        this.engine = engine;
+        this.horsePower = horsePower;
+    }
+
 //    public Model(String name, String engine, int horsePower, Brand brand) {
 //        this(name, engine, horsePower);
 //        this.brand = brand;
 //    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
 //    public Brand getBrand() {
 //        return brand;
 //    }
@@ -47,29 +49,29 @@
 //    public void setBrand(Brand brand) {
 //        this.brand = brand;
 //    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getEngine() {
-//        return engine;
-//    }
-//
-//    public void setEngine(String engine) {
-//        this.engine = engine;
-//    }
-//
-//    public int getHorsePower() {
-//        return horsePower;
-//    }
-//
-//    public void setHorsePower(int horsePower) {
-//        this.horsePower = horsePower;
-//    }
-//}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}
 
