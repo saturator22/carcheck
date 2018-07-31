758cb7ccc2f991660c322a3cdb7e50704326691c
Bart <bartpetka@gmail.com>
2018-07-31 09:53:37 +0200

    Implement brand class

--- src/main/java/com/codecool/model/Brand.java
+++ src/main/java/com/codecool/model/Brand.java
@@ -1,6 +1,30 @@
 package com.codecool.model;
 
-import javax.servlet.http.HttpServlet;
+import javax.persistence.*;
+import java.util.List;
 
+@Entity(name="brands")
 public class Brand{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int brand_id;
+    private String name;
+    @OneToMany
+    private List<Model> models;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Model> getModels() {
+        return models;
+    }
+
+    public void setModels(List<Model> models) {
+        this.models = models;
+    }
 }
