45fd6e019bf55f5164c1b99040c3a7b0fac83e8e
reggae-krk <monika.gorecka.agh@gmail.com>
2018-07-31 09:46:46 +0200

    Add fields and constructors to Producer class

--- src/main/java/com/codecool/model/Producer.java
+++ src/main/java/com/codecool/model/Producer.java
@@ -3,4 +3,18 @@ package com.codecool.model;
 import javax.servlet.http.HttpServlet;
 
 public class Producer {
+
+    private long id;
+
+    private String producerName;
+
+    public Producer(long id, String producerName) {
+        this.id = id;
+        this.producerName = producerName;
+    }
+
+    public Producer() {
+
+    }
+
 }
