3a4d67cafa0abd3f1881d42fc1972e6f2eb1c167
saturator22 <f.szczeski@gmail.com>
2018-07-31 09:37:09 +0200

    Implement Annotatations to Pojo model class

---
+++ src/main/java/com/codecool/model/Model.java
@@ -0,0 +1,22 @@
+package com.codecool.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Model{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "brandId")
+    private long brandId;
+
+    private String name;
+
+    private String engine;
+
+    private int horsePower;
+
+
+}
+
