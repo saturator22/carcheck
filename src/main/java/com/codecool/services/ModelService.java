bffce87f772958e7446fed9e95ee7ad83a10ea70
saturator22 <f.szczeski@gmail.com>
2018-08-01 22:04:52 +0200

    Implement getModelById and saveModel methods

--- src/main/java/com/codecool/services/ModelService.java
+++ src/main/java/com/codecool/services/ModelService.java
@@ -1,5 +1,6 @@
 package com.codecool.services;
 
+import com.codecool.model.Brand;
 import com.codecool.model.Model;
 
 import javax.persistence.EntityManager;
@@ -14,10 +15,23 @@ public class ModelService {
         this.em = em;
     }
 
-    public List<Model> getAllModels(Integer id) {
-        List<Model> models = em.createQuery("SELECT m FROM Model m WHERE brand_id IN :id").setParameter("id", id).getResultList();
+    public List<Model> getAllModels(Long id) {
+        List<Model> models = em.createQuery("SELECT b.models FROM Brand b WHERE b.id=:id")
+                .setParameter("id", id)
+                .getResultList();
         return models;
     }
 
+    public Model getModelById(Long id) {
+        return em.find(Model.class, id);
+    }
 
+    public void saveModel(Long id, Model model) {
+        System.out.println(model.toString());
+        Brand brand = em.find(Brand.class, id);
+        model.setBrand(brand);
+        em.getTransaction().begin();
+        em.persist(model);
+        em.getTransaction().commit();
+    }
 }
