886ec94f84fa2ba40d8e630294ffd0f3349d4b6e
reggae-krk <monika.gorecka.agh@gmail.com>
2018-08-02 00:39:32 +0200

    Implement constructor with EntityManager etc

--- src/main/java/com/codecool/services/ProducerService.java
+++ src/main/java/com/codecool/services/ProducerService.java
@@ -1,52 +1,20 @@
 package com.codecool.services;
 
-import com.codecool.model.Brand;
 import com.codecool.model.Producer;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.*;
+import javax.persistence.*;
+import java.util.List;
 
 public class ProducerService {
 
-//    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("carcheck");
-//    private EntityManager em = emf.createEntityManager();
-
-    private Map<Long, Producer> producers = new HashMap<>();
+    private EntityManagerFactory emf;
+    private EntityManager em;
+    private EntityTransaction transaction;
 
     public ProducerService() {
-//        Set<Brand> brandList = new HashSet<>();
-//        brandList.add(new Brand("test1"));
-//        brandList.add(new Brand("test2"));
-//
-//        producers.put(1L, new Producer(1, "bmw", brandList));
-//        producers.put(2L, new Producer(2, "mazda", brandList));
+        this.emf = Persistence.createEntityManagerFactory("carcheck");
+        this.em = emf.createEntityManager();
+        this.transaction = em.getTransaction();
     }
 
-    public List<Producer> getAllProducers() {
-        return new ArrayList<Producer>(producers.values());
-    }
-
-    public Producer getProducer(long id) {
-        return producers.get(id);
-    }
-
-    public Producer addProducer(Producer producer) {
-        producer.setId(producers.size() + 1);
-        producers.put(producer.getId(), producer);
-        return producer;
-    }
-
-    public Producer updateProducer(Producer producer) {
-        if (producer.getId() <= 0) {
-            return null;
-        }
-        producers.put(producer.getId(), producer);
-        return producer;
-    }
-
-    public Producer removeProducer(long id) {
-        return producers.remove(id);
-    }
 }
