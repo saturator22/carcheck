53df6e56277b90d3327cefe5c78a8ea66b20a9bb
reggae-krk <monika.gorecka.agh@gmail.com>
2018-08-01 09:46:06 +0200

    Create services package with ProducerServices class

---
+++ src/main/java/com/codecool/services/ProducerService.java
@@ -0,0 +1,12 @@
+package com.codecool.services;
+
+import com.codecool.model.Producer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProducerService {
+
+}
