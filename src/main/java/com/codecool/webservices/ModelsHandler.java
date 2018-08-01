a9f0d938ae12bc8cdfbdd516fdb92588c76bbaa9
saturator22 <f.szczeski@gmail.com>
2018-08-01 22:05:50 +0200

    Little code refactor + @POST method body

--- src/main/java/com/codecool/webservices/ModelsHandler.java
+++ src/main/java/com/codecool/webservices/ModelsHandler.java
@@ -3,7 +3,9 @@ package com.codecool.webservices;
 import com.codecool.model.Brand;
 import com.codecool.model.Model;
 import com.codecool.model.Producer;
+import com.codecool.services.ModelService;
 
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,22 +18,38 @@ import java.util.*;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ModelsHandler {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("carcheck");
-    EntityManager em = emf.createEntityManager();
+    EntityManager em;
+    ModelService modelService = new ModelService(getEntityManager());
 
     @GET
     public List<Model> listModels(@PathParam("id") Long brandId) {
-        Query query = em.createQuery("SELECT b.models FROM Brand b WHERE b.id=:id");
-        query.setParameter("id", brandId);
-
-        List<Model> modelsCollection = query.getResultList();
-
-        return modelsCollection;
+        em = getEntityManager();
+        List<Model> modelsById = modelService.getAllModels(brandId);
+        closeManagers();
+        return modelsById;
     }
 
-//    @GET
-//    @Path("{id}")
-//    public Model getModelById(@PathParam("id") Integer modelId) {
-//        return em.find(Model.class, modelId);
-//    }
+    @GET
+    @Path("{id}")
+    public Model getModelById(@PathParam("id") Long modelId) {
+        em = getEntityManager();
+        Model model = modelService.getModelById(modelId);
+        closeManagers();
+        return model;
+    }
+    //TODO
+    @POST
+    public void addModel(@PathParam("id") Long id, Model model) {
+        em = getEntityManager();
+        modelService.saveModel(id, model);
+    }
+
+    protected EntityManager getEntityManager() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("carcheck");
+        return emf.createEntityManager();
+    }
+
+    private void closeManagers() {
+        em.close();
+    }
 }
