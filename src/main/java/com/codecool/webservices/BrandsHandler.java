98a61cefec913f164a59f6d7be0cdd318dbbe8bf
Bart <bartpetka@gmail.com>
2018-08-01 11:00:42 +0200

    Implement deleteBrand method

--- src/main/java/com/codecool/webservices/BrandsHandler.java
+++ src/main/java/com/codecool/webservices/BrandsHandler.java
@@ -6,10 +6,7 @@ import com.codecool.services.BrandService;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/brands")
@@ -28,6 +25,12 @@ public class BrandsHandler {
     public Brand getBrandById(long id) {
         Brand brand = bs.findBrand(id);
         return brand;
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteBrand(@PathParam("id") long id) {
+        bs.removeBrand(id);
     }
 
 }
