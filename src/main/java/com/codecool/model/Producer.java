3e6a224383aef0a1c6994749b0c6d86abdb2a622
reggae-krk <monika.gorecka.agh@gmail.com>
2018-08-01 11:21:20 +0200

    Add brandList to Producer class

--- src/main/java/com/codecool/model/Producer.java
+++ src/main/java/com/codecool/model/Producer.java
@@ -1,17 +1,26 @@
 package com.codecool.model;
 
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.HashSet;
+import java.util.Set;
 
+//@Entity(name = "producer")
 @XmlRootElement
 public class Producer {
 
+  //  @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String producerName;
 
-    public Producer(long id, String producerName) {
-        this.id = id;
+    //@OneToMany(cascade = {CascadeType.ALL}, mappedBy = "producer")
+    private Set<Brand> brandsList = new HashSet<>();
+
+    public Producer(String producerName, Set<Brand> brandList) {
         this.producerName = producerName;
+        this.brandsList = brandList;
     }
 
     public Producer() {
@@ -32,5 +41,21 @@ public class Producer {
 
     public void setProducerName(String producerName) {
         this.producerName = producerName;
+    }
+
+    public Set<Brand> getBrandsList() {
+        return brandsList;
+    }
+
+    public void setBrandsList(Set<Brand> brandsList) {
+        this.brandsList = brandsList;
+    }
+
+    public void addBrand(Brand brand) {
+        brandsList.add(brand);
+    }
+
+    public void removeBrand(Brand brand) {
+        brandsList.remove(brand);
     }
 }
