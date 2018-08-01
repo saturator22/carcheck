15b0b716bfc1edffba43549c08a01b5e1932f365
Bart <bartpetka@gmail.com>
2018-08-01 10:55:59 +0200

    Implement getBrandById method

--- src/main/java/com/codecool/webservices/BrandsHandler.java
+++ src/main/java/com/codecool/webservices/BrandsHandler.java
@@ -1,4 +1,33 @@
 package com.codecool.webservices;
 
+import com.codecool.model.Brand;
+import com.codecool.services.BrandService;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/brands")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class BrandsHandler {
+    BrandService bs;
+
+    public BrandsHandler() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("carcheck");
+        EntityManager em = emf.createEntityManager();
+        this.bs = new BrandService(em);
+    }
+
+    @GET
+    public Brand getBrandById(long id) {
+        Brand brand = bs.findBrand(id);
+        return brand;
+    }
+
 }
