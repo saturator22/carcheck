5819d15eca495e9aa66bfe025f4ee97e812b57f0
reggae-krk <monika.gorecka.agh@gmail.com>
2018-08-01 09:42:55 +0200

    Add GET annotation with implementation

--- src/main/java/com/codecool/webservices/ProducersHandler.java
+++ src/main/java/com/codecool/webservices/ProducersHandler.java
@@ -1,6 +1,22 @@
 package com.codecool.webservices;
 
-import javax.ws.rs.GET;
+import com.codecool.model.Producer;
+import com.codecool.services.ProducerService;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/producers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ProducersHandler {
+
+    private ProducerService producerService = new ProducerService();
+
+    @GET
+    public List<Producer> getProducers() {
+        return producerService.getAllProducers();
+    }
+
 }
