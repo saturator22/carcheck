0f7e5d97dddc75e4a867ad42f11d1aa751edb4c2
reggae-krk <monika.gorecka.agh@gmail.com>
2018-08-01 09:47:13 +0200

    Add constructor with simple mock data

--- src/main/java/com/codecool/services/ProducerService.java
+++ src/main/java/com/codecool/services/ProducerService.java
@@ -9,4 +9,11 @@ import java.util.Map;
 
 public class ProducerService {
 
+    private Map<Long, Producer> producers = new HashMap<>();
+
+    public ProducerService() {
+        producers.put(1L, new Producer(1, "bmw"));
+        producers.put(2L, new Producer(2, "mazda"));
+    }
+
 }
