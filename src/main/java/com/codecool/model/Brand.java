1efadf487fc463476aac7b1949ed95bea3949f72
Bart <bartpetka@gmail.com>
2018-08-01 09:57:41 +0200

    Bug fix

--- src/main/java/com/codecool/model/Brand.java
+++ src/main/java/com/codecool/model/Brand.java
@@ -1,6 +1,16 @@
 package com.codecool.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity(name = "brand")
+@XmlRootElement
 public class Brand{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String brandName;
