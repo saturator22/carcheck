a6001c5460fdfe68702fc326abf3e658894debe8
reggae-krk <monika.gorecka.agh@gmail.com>
2018-08-02 18:36:41 +0200

    Add annotations to produce text_html and implement response

--- src/main/java/com/codecool/webservices/ProducersHandler.java
+++ src/main/java/com/codecool/webservices/ProducersHandler.java
@@ -5,38 +5,68 @@ import com.codecool.services.ProducerService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/producers")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+
 public class ProducersHandler {
 
     private ProducerService producerService = new ProducerService();
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Producer> getProducers() {
         return producerService.getAllProducers();
     }
 
     @POST
-    public Producer addProducer(Producer producer) {
-        return producerService.addProducer(producer);
+    @Produces(MediaType.TEXT_HTML)
+    public Response addProducer(Producer producer) {
+        Producer newProducer = producerService.addProducer(producer);
+
+        return Response.status(Response.Status.CREATED)// 201
+                .entity("A new producer has been created")
+                .header("Location",
+                        "http://localhost:8080/producers/"
+                                + String.valueOf(newProducer.getId())).build();
     }
 
     @PUT
+    @Produces(MediaType.TEXT_HTML)
     @Path("/{producerId}")
-    public Producer updateProducer(@PathParam("producerId") long id, Producer producer) {
-        return producerService.updateProducer(id, producer);
+    public Response updateProducer(@PathParam("producerId") long id, Producer producer) {
+        Producer newProducer = producerService.updateProducer(id, producer);
+
+        return Response.status(Response.Status.CREATED)// 201
+                .entity("Producer has been updated")
+                .header("Location",
+                        "http://localhost:8080/producers/"
+                                + String.valueOf(newProducer.getId())).build();
     }
 
     @DELETE
+    @Produces(MediaType.TEXT_HTML)
     @Path("/{producerId}")
-    public void deleteProducer(@PathParam("producerId") long id) {
-        producerService.removeProducer(id);
+    public Response deleteProducer(@PathParam("producerId") long id) {
+
+        if(producerService.removeProducer(id) == true) {
+            return Response.status(Response.Status.OK)// 201
+                    .entity("Producer has been deleted")
+                    .header("Location",
+                            "http://localhost:8080/producers/"
+                                    + String.valueOf(id)).build();
+        }
+        else return Response.status(Response.Status.BAD_REQUEST)
+                .entity("Producer hasn't been deleted")
+                .header("Location",
+                        "http://localhost:8080/producers/"
+                                    + String.valueOf(id)).build();
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/{producerId}")
     public Producer getProducer(@PathParam("producerId") long id) {
         return producerService.getProducer(id);
