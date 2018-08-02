1871b53c9fdc191f2369cb9aaf0ddb2e21c6b32d
saturator22 <f.szczeski@gmail.com>
2018-08-02 15:17:54 +0200

    Implement Update & Put methods

--- src/main/java/com/codecool/services/ModelService.java
+++ src/main/java/com/codecool/services/ModelService.java
@@ -4,15 +4,28 @@ import com.codecool.model.Brand;
 import com.codecool.model.Model;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.EntityTransaction;
 import java.util.List;
 
 public class ModelService {
         EntityManager em;
+        EntityTransaction transaction;
 
     public ModelService(EntityManager em) {
         this.em = em;
+        this.transaction = em.getTransaction();
+    }
+
+    public Model deleteModel(Long id, Long brandId) {
+        Model modelToDelete = em.find(Model.class, id);
+        modelToDelete.setBrand(null);
+
+        transaction.begin();
+        em.remove(modelToDelete);
+        transaction.commit();
+
+        em.close();
+        return modelToDelete;
     }
 
     public List<Model> getAllModels(Long id) {
@@ -22,16 +35,39 @@ public class ModelService {
         return models;
     }
 
-    public Model getModelById(Long id) {
-        return em.find(Model.class, id);
+    public Model getModelById(Long id, Long brandId) {
+        Brand brand = em.find(Brand.class, brandId);
+        Model model = em.find(Model.class, id);
+
+        if(model.getBrand().getId() == brand.getId()) {
+            return model;
+        }
+        return null;
     }
 
-    public void saveModel(Long id, Model model) {
-        System.out.println(model.toString());
+    public Model saveModel(Long id, Model model) {
         Brand brand = em.find(Brand.class, id);
         model.setBrand(brand);
-        em.getTransaction().begin();
+        transaction.begin();
         em.persist(model);
-        em.getTransaction().commit();
+        transaction.commit();
+        closeManagers();
+        return model;
+    }
+
+    public Model updateModel(Long id, Model model) {
+        Model modelToUpdate = em.find(Model.class, id);
+        modelToUpdate.setName(model.getName());
+        modelToUpdate.setHorsePower(model.getHorsePower());
+        modelToUpdate.setEngine(model.getEngine());
+        transaction.begin();
+        em.merge(modelToUpdate);
+        transaction.commit();
+        closeManagers();
+        return modelToUpdate;
+    }
+
+    private void closeManagers() {
+        em.close();
     }
 }
